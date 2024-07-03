6c810a897792d5e571854efee38de2b6178e1390
seulgiishin <tls940807@gmail.com>
2024-07-03 14:53:08 +0900

    [refactoring] page 51

--- 0703_BaseballKATA/Baseball.cpp
+++ 0703_BaseballKATA/Baseball.cpp
@@ -15,7 +15,10 @@ public:
 
 	GuessResult guess(const string& guessNumber) {
 		assrtIllegalArgument(guessNumber);
-		return { true,3,0 };
+		if (guessNumber == question) {
+			return { true,3,0 };
+		}
+		return { false,0,0 };
 	}
 
 	void assrtIllegalArgument(const std::string& guessNumber)
